8f62adef965e1c8e573c0b16d9a70e9bddd0d235
Valencia Lie <valencialie11@yahoo.com>
2020-07-03 04:14:19 +0700

    screwed up structure, prob gotta redo

--- textbasedgame.py
+++ textbasedgame.py
@@ -32,60 +32,75 @@ def firstsegment():
     print("1. You sit quietly at the corner of the room and started to wait for Mr Avery to tell you what to do.")
     print("2. You yell out of frustration because the phone Mr Avery found is not yours.")
     print("3. You head towards one of the students and ask whether this is a mistake.")
-    try:
-        firstchoice = int(input())
-    except ValueError:
-        print("Please enter just the number (1,2 or 3) of your choice.")
-    
-    if firstchoice == 1:
-        avery = print("Suddenly, Mr Avery came. He handed each person a paper and asked everyone to write 500 essay on the negative impacts of technology on teens in America. What would you do next?")
-    elif firstchoice == 2:
-        print('"AAAAAAHHHH!!" you shouted. The other 5 people looked at you strangely and started asking you who you are. Apparently, they have never even known your existence before. What do you do next?')
-        print("1. Introduce your name.")
-        print('2. Say incredulously, "How is it that you guys dont know who I am?"')
-        print("3. Ignore their question.")
+    while True:
         try:
-            action = int(input())
-        except ValueError:
-            print("Please enter just the number (1, 2, 3, 4 or 5) of your choice.")
-        if action == 1:
-            print("My name is " + name + ". Nice to meet you guys, I guess. I've been here since freshman though...")
-            avery
-        elif action == 2:
-            print('They rolled their eyes and said, "How are we supposed to know you when youre as mute as a mouse?!"')
-            avery
-        else:
-            avery
-    else:
-        print("Who do you ask among the 5 people?")
-        print("1. Bronwyn Rojas")
-        print("2. Addy Prentiss")
-        print("3. Nate Macauley")
-        print("4. Cooper Clay")
-        print("5. Simon Kelleher")
-        try:
-            who = int(input())
-        except ValueError:
-            print("Please enter just the number (1, 2, 3, 4 or 5) of your choice.")
+            firstchoice = int(input())
+            if firstchoice == 1:
+                print("Suddenly, Mr Avery came. He handed each person a paper and asked everyone to write 500 essay on the negative impacts of technology on teens in America. What would you do next?")
+
+            elif firstchoice == 2:
+                print('"AAAAAAHHHH!!" you shouted. The other 5 people looked at you strangely and started asking you who you are. Apparently, they have never even known your existence before. What do you do next?')
+                print("1. Introduce your name.")
+                print('2. Say incredulously, "How is it that you guys dont know who I am?"')
+                print("3. Ignore their question.")
+                while True:
+                    try:
+                        action = int(input())
+                        if action == 1:
+                            print('"My name is " + name + ". Nice to meet you guys, I guess. Ive been here since freshman though..." you said silently.')
+                            print("Suddenly, Mr Avery came. He handed each person a paper and asked everyone to write 500 essay on the negative impacts of technology on teens in America. What would you do next?")
+                        elif action == 2:
+                            print('They rolled their eyes and said, "How are we supposed to know you when youre as mute as a mouse?!"')
+                            print("Suddenly, Mr Avery came. He handed each person a paper and asked everyone to write 500 essay on the negative impacts of technology on teens in America. What would you do next?")
+                        elif action == 3:
+                            print("Suddenly, Mr Avery came. He handed each person a paper and asked everyone to write 500 essay on the negative impacts of technology on teens in America. What would you do next?")
+                        else:
+                            print("Please enter just the number (1, 2, 3, 4 or 5) of your choice.")
+                    except ValueError:
+                        print("Please enter just the number (1, 2, 3, 4 or 5) of your choice.")
         
-        if who == 1:
-            print('"Do I look like I purposely bring a phone to Mr Averys class too? Im not dumb. Of course this is a mistake!", says Bronwyn.')
-            avery
-        elif who == 2:
-            print('"I think so. Im supposed to go and see my boyfriend Jake after class. Dang it Mr Avery!", says Addy.')
-            avery
-        elif who == 3:
-            print('"Ya. Im busy enough with my customers and I dont need this kind of bs."')
-            avery
-        elif who == 4:
-            print('"Yes. Its not my phone either. We gotta tell Mr Avery!"')
-            avery
-        else:
-            print('"Duh! I will make sure to feature Mr Avery on ABOUT THAT and make him pay!"')
-            avery
+            elif firstchoice == 3:
+                print("Who do you ask among the 5 people?")
+                print("1. Bronwyn Rojas")
+                print("2. Addy Prentiss")
+                print("3. Nate Macauley")
+                print("4. Cooper Clay")
+                print("5. Simon Kelleher")
+                while True:
+                    try:
+                        who = int(input())
+                        if who == 1:
+                            print('"Do I look like I purposely bring a phone to Mr Averys class too? Im not dumb. Of course this is a mistake!", says Bronwyn.')
+                            print("Suddenly, Mr Avery came. He handed each person a paper and asked everyone to write 500 essay on the negative impacts of technology on teens in America. What would you do next?")
+                        elif who == 2:
+                            print('"I think so. Im supposed to go and see my boyfriend Jake after class. Dang it, Mr Avery!", says Addy.')
+                            print("Suddenly, Mr Avery came. He handed each person a paper and asked everyone to write 500 essay on the negative impacts of technology on teens in America. What would you do next?")
+                        elif who == 3:
+                            print('"Ya. Im busy enough with my customers and I dont need this kind of bs."')
+                            print("Suddenly, Mr Avery came. He handed each person a paper and asked everyone to write 500 essay on the negative impacts of technology on teens in America. What would you do next?")
+                        elif who == 4:
+                            print('"Yes. Its not my phone either. We gotta tell Mr Avery!"')
+                            print("Suddenly, Mr Avery came. He handed each person a paper and asked everyone to write 500 essay on the negative impacts of technology on teens in America. What would you do next?")
+                        elif who == 5:
+                            print('"Duh! I will make sure to feature Mr Avery on ABOUT THAT and make him pay!"')
+                            print("Suddenly, Mr Avery came. He handed each person a paper and asked everyone to write 500 essay on the negative impacts of technology on teens in America. What would you do next?")
+                        else:
+                            print("Please enter just the number (1, 2, 3, 4 or 5) of your choice.")
+                    except ValueError:
+                        print("Please enter just the number (1, 2, 3, 4 or 5) of your choice.")
+            else:
+                print("Please enter just the number (1,2 or 3) of your choice.")
+
+        except ValueError:
+            print("Please enter just the number (1,2 or 3) of your choice.")
+    
+   
 
 
-
+if __name__== "__main__":
+  intro()
+  choose()
+  firstsegment()
 
 
 
